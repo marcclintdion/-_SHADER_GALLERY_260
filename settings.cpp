a0e61c0ec0ea72b4a163279ebd52ce89c3e495ed
Marc Clint Dion <marcclintdion@Marcs-iMac.local>
2015-09-17 22:06:09 -0700

    “init”

---
+++ settings.cpp
@@ -0,0 +1,11 @@
+var         shinyPlasticFireBall_SECOND_SPECULAR[]    =  {1.37, 1, 1.17, 1.0};
+
+var         shinyPlasticFireBall_ATTENUATION      =  1.977;
+
+var         refraction_SHININESS                  =  1.00098;
+var         refraction_ATTENUATION                =  0.78;
+
+
+          moveSet[0]    =  1.5;
+          moveSet[1]    =  1.9;
+          moveSet[2]    =  -7.1;
